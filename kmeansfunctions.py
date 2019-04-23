cc8486653cda5ac3a18898a508e5ead501d26cfc
eric-yim <48815706+eric-yim@users.noreply.github.com>
2019-04-23 13:26:49 -0500

    Create kmeansfunctions.py

---
+++ kmeansfunctions.py
@@ -0,0 +1,45 @@
+#Original code from https://databricks.com/tensorflow/clustering-and-k-means
+import tensorflow as tf
+import numpy as np
+
+def choose_random_centroids(samples, n_clusters):
+    # Step 0: Initialisation: Select `n_clusters` number of random points
+    n_samples = tf.shape(samples)[0]
+    random_indices = tf.random_shuffle(tf.range(0, n_samples))
+    begin = [0,]
+    size = [n_clusters,]
+    size[0]= n_clusters
+    centroid_indices = tf.slice(random_indices, begin, size)
+    initial_centroids = tf.gather(samples, centroid_indices)
+    return initial_centroids
+def assign_to_nearest(samples, centroids):
+    # Finds the nearest centroid for each sample
+
+    # START from http://esciencegroup.com/2016/01/05/an-encounter-with-googles-tensorflow/
+    expanded_vectors = tf.expand_dims(samples, 0)
+    expanded_centroids = tf.expand_dims(centroids, 1)
+    distances = tf.reduce_sum( tf.square(
+               tf.subtract(expanded_vectors, expanded_centroids)), 2)
+    mins = tf.argmin(distances, 0)
+    # END from http://esciencegroup.com/2016/01/05/an-encounter-with-googles-tensorflow/
+    nearest_indices = mins
+    return nearest_indices, tf.reduce_min(distances,0)
+def assign_to_nearest2(samples, centroids):
+    # Finds the nearest centroid for each sample
+
+    # START from http://esciencegroup.com/2016/01/05/an-encounter-with-googles-tensorflow/
+    expanded_vectors = tf.expand_dims(samples, 0)
+    expanded_centroids = tf.expand_dims(centroids, 1)
+    distances = tf.reduce_sum( tf.square(
+               tf.subtract(expanded_vectors, expanded_centroids)), 2)
+    mins = tf.argmin(distances, 0)
+    # END from http://esciencegroup.com/2016/01/05/an-encounter-with-googles-tensorflow/
+    nearest_indices = mins
+    return nearest_indices, distances,expanded_vectors,expanded_centroids,tf.subtract(expanded_vectors,expanded_centroids)
+
+def update_centroids(samples, nearest_indices, n_clusters):
+    # Updates the centroid to be the mean of all samples associated with it.
+    nearest_indices = tf.to_int32(nearest_indices)
+    partitions = tf.dynamic_partition(samples, nearest_indices, n_clusters)
+    new_centroids = tf.concat([tf.expand_dims(tf.reduce_mean(partition, 0), 0) for partition in partitions], 0)
+    return new_centroids    
